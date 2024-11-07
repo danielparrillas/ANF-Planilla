2b05897fdcce6dbc59acba40cd5b17fb864b7e18
daniel <daniel16parrillas@gmail.com>
2024-11-06 18:02:00 -0600

    build: :building_construction: cambiar a entorno de escritorio

--- desktop/src/renderer/src/pages/reportes/reporte-form.tsx
+++ desktop/src/renderer/src/pages/reportes/reporte-form.tsx
@@ -1,15 +1,15 @@
 import { useParams } from 'react-router-dom'
 
-function EmpleadoForm() {
+function ReporteForm() {
   const { id } = useParams() // Captura el parámetro de la URL
   const isEditing = id !== 'nuevo' // Si el id es "nuevo", es una creación; si no, es edición.
 
   return (
     <div>
-      <h1>{isEditing ? 'Editar Empleado' : 'Crear Empleado'}</h1>
+      <h1>{isEditing ? 'Editar Reporte' : 'Crear Reporte'}</h1>
       {/* Aquí va el formulario */}
     </div>
   )
 }
 
-export default EmpleadoForm
+export default ReporteForm
