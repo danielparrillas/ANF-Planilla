dc9890c7dbcdad1b0acacac4c9ec7a59d46869f4
danielparrillas <daniel16parrillas@gmail.com>
2024-11-14 09:06:21 -0600

    build: :rocket: configurar para despliegue

--- src/main.tsx
+++ src/main.tsx
@@ -1,58 +1,63 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './pages/home/home-page'
-import LoginPage from './pages/login/login-page'
-import EmpleadoList from './pages/empleados/empleado-list'
-import EmpleadoForm from './pages/empleados/empleado-form'
-import PlanillaList from './pages/planillas/planilla-list'
-import PlanillaForm from './pages/planillas/planilla-form'
-import ReporteList from './pages/reportes/reporte-list'
-import ReporteForm from './pages/reportes/reporte-form'
-import NotFoundPage from './pages/notFoundPage/not-found-page'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home/home-page";
+import LoginPage from "./pages/login/login-page";
+import EmpleadoList from "./pages/empleados/empleado-list";
+import EmpleadoForm from "./pages/empleados/empleado-form";
+import PlanillaList from "./pages/planillas/planilla-list";
+import PlanillaForm from "./pages/planillas/planilla-form";
+import ReporteList from "./pages/reportes/reporte-list";
+import ReporteForm from "./pages/reportes/reporte-form";
+import NotFoundPage from "./pages/notFoundPage/not-found-page";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />
-  },
-  {
-    path: '/login',
-    element: <LoginPage />
-  },
-  {
-    path: '/empleados',
-    element: <EmpleadoList />
-  },
-  {
-    path: '/empleado/:id', // Para crear o editar empleados
-    element: <EmpleadoForm />
-  },
-  {
-    path: '/planillas',
-    element: <PlanillaList />
-  },
-  {
-    path: '/planilla/:id', // Para crear o editar planillas
-    element: <PlanillaForm />
-  },
-  {
-    path: '/reportes',
-    element: <ReporteList />
-  },
-  {
-    path: '/reporte/:id', // Para crear o editar reportes
-    element: <ReporteForm />
-  },
-  {
-    path: '*', // Ruta de error 404
-    element: <NotFoundPage />
-  }
-])
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <HomePage />,
+		},
+		{
+			path: "/login",
+			element: <LoginPage />,
+		},
+		{
+			path: "/empleados",
+			element: <EmpleadoList />,
+		},
+		{
+			path: "/empleado/:id", // Para crear o editar empleados
+			element: <EmpleadoForm />,
+		},
+		{
+			path: "/planillas",
+			element: <PlanillaList />,
+		},
+		{
+			path: "/planilla/:id", // Para crear o editar planillas
+			element: <PlanillaForm />,
+		},
+		{
+			path: "/reportes",
+			element: <ReporteList />,
+		},
+		{
+			path: "/reporte/:id", // Para crear o editar reportes
+			element: <ReporteForm />,
+		},
+		{
+			path: "*", // Ruta de error 404
+			element: <NotFoundPage />,
+		},
+	],
+	{
+		basename: "/ANF-Planilla",
+	}
+);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
